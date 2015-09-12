c87570ccf49984cac309d25266e4779795eae0a5
joolsmf <joolsmf@gmail.com>
2015-09-12 14:39:24 +0100

    Further Javadoc tidying up.

--- src/main/rhythm/analysis/model/stringHierachyAnalysis/LevenshteinAnalyser.java
+++ src/main/rhythm/analysis/model/stringHierachyAnalysis/LevenshteinAnalyser.java
@@ -12,31 +12,38 @@ import org.apache.commons.lang3.StringUtils;
 /**
  * 
  * Utility class for Arc Analysis containing one public static method getDifference. 
- * Note - This implementation is a HIGHLY inefficient, 'naive' implementation, and serves only as a simple heuristic proof of concept.
- * Intended for strings of length < 1000
+ * Note - This implementation is a HIGHLY inefficient, 'naive' implementation, and serves only as a proof of concept.
+ * Intended for strings of less than one thousand characters in length
  *
  */
 public class LevenshteinAnalyser {
 	
-	
-	private static double getDifference(String src, String arc){
-		double difference = StringUtils.getLevenshteinDistance(src, arc);
-		return (difference / (src.length()));
-		
+	/**
+	 * Get difference gets the Levenshtein between two strings divided by the length of the first string. 
+	 * Intended for use on strings of equal length.
+	 * @param the first string to be compared
+	 * @param the second string to be compared
+	 * @return the Levenshtein distance between the two string parameters divided by the length of the first string
+	 */
+	public static double getDifference(String string1, String string2){
+		double difference = StringUtils.getLevenshteinDistance(string1, string2);
+		return (difference / (string1.length()));
 	}
 	
 	private static List<String> valueToString(String value){
 		List<String> list = new ArrayList<String>();
 		char[] charArray = value.toCharArray();
-		
 		for(int i = charArray.length -1; i > 0; i--) list.add(value.substring(0, i));
-			
 		return list;
 	}
 	
-	// Make sure not intersecting
-	// If score below a certain threshold then merge indices together (using set so not keeping dupes)
 	
+	/**
+	 *  Takes a map of substrings to indices and returns a map of similar substrings to indices.
+	 *  Highly inefficient prototype implementation due to deeply nested loops
+	 * @param Map of substrings to indices
+	 * @return Map of similar substrings to indices
+	 */
 	public static Map<String, List<Integer>> findSimilarStrings(Map<String, List<Integer>> map ) {
 		Map<String, List<Integer>> result = new TreeMap<String, List<Integer>>();
 		
--- src/main/rhythm/analysis/model/stringHierachyAnalysis/StringHierarchyAnalyser.java
+++ src/main/rhythm/analysis/model/stringHierachyAnalysis/StringHierarchyAnalyser.java
@@ -4,34 +4,29 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 import java.util.TreeMap;
 
-import rhythm.analysis.control.RhythmController;
 import rhythm.analysis.model.suffixTree.SuffixTree;
 
 
 
 /**
+ * StringHierarchyAnalyser Class responsible for analysing raw output of SuffixTree and returning data
+ * for suitable for rendering an arc diagram.
  * 
- * Class responsible for analysing raw output of SuffixTree and returning data
- * for returning data suitable for rendering arc diagram in view.
- *
+ * @author Julian Fenner
  */
-//TO DO - Once prototype working will need improving as expensive computation involving lots of 
-// reprocessing of maps
+
 public class StringHierarchyAnalyser {
-	
-	//private SuffixTree suffixTree;
 	private List<List<Integer>> stringPairData;
 	
 	
+	/*-----------------------------------------------------------------------------------------
+	 * Constructors
+	 *----------------------------------------------------------------------------------------*/
 	public StringHierarchyAnalyser(){
-		
 	}
 	
 	public StringHierarchyAnalyser(SuffixTree suffixTree){
@@ -41,24 +36,29 @@ public class StringHierarchyAnalyser {
 		
 	
 	
-	
+	/**
+	 * Gets the matching strings
+	 * @param the suffix tree map data to be analysed
+	 * @param boolean value controlling whether analysis should be applied
+	 * @return List of Integer List representing the matching strings
+	 */
 	public List<List<Integer>> getStringCoordinatesExactMatch(Map<String, List<Integer>> suffixMap, boolean applyStringAnalysis){
+		//TO DO - Refactor so that applyStringAnalysis parameter not necessary
 		return getStringCoordinates(suffixMap, applyStringAnalysis);
 	}
 	
+	/**
+	 * Gets the similar strings
+	 * @param the suffix tree map data to be analysed
+	 * @param boolean value controlling whether analysis should be applied
+	 * @return List of Integer List representing the similar strings
+	 */
 	public List<List<Integer>> getStringCoordinatesInexactMatch(Map<String, List<Integer>> suffixMap, boolean applyStringAnalysis){
-		//LevenshteinArc.getDifference(src, arc)
-		//process suffix map through LevenshteinAnalyser to get similar matches
+		//TO DO - Refactor so that applyStringAnalysis parameter not necessary
 		return getStringCoordinates(LevenshteinAnalyser.findSimilarStrings(suffixMap),applyStringAnalysis);
 	}
 	
-	
-	/**
-	 * Processes map "Substring -> Indices" and processes as follows
-	 * 1. Remove overlapping substrings 
-	 * 2. If substring is of length > 1 and has more that one index (i.e it repeats) then create
-	 *    coordinates
-	 */
+		
 	private List<List<Integer>>  getStringCoordinates(Map<String, List<Integer>> suffixMap,  boolean applyStringAnalysis){
 		stringPairData = new ArrayList<List<Integer>>(); // for return
 		List<StringPair> stringPairs = new ArrayList<StringPair>();
@@ -68,14 +68,13 @@ public class StringHierarchyAnalyser {
 			List<Integer> value = entry.getValue();
 			
 			if(value.size() > 0){  // keep repeating substrings only		
+				
 				for(int i = 0; i < value.size() -1; i++){
 					
 					StringPair stringPair = new StringPair(value.get(i), value.get(i+1), keyLength, key);
 
-					//TO DO pass in boolean test as function arg so that behaviour of method can be better modified and updated
 					if(! applyStringAnalysis || stringValid(stringPairs, stringPair)){
 						stringPairs.add(stringPair);
-						//to do use arcPair below
 						stringPairData.add(Arrays.asList(value.get(i), value.get(i) + (keyLength -1),
 												  value.get(i+1), value.get(i+1) + (keyLength -1)));
 					} 
@@ -96,7 +95,7 @@ public class StringHierarchyAnalyser {
 		return true;
 	}
 	
-	/**
+	/*
 	 * . Removes overlapping substrings e.g. take String ABABABAB
 	 * 	 ABAB appears at 0,2,4.  However, ABAB at 2 intersects ABAB at 0
 	 *   so it is discarded
@@ -107,39 +106,29 @@ public class StringHierarchyAnalyser {
 	 *   
 	 *   Sorts by key length in descending order
 	 */
-	public TreeMap <String, List<Integer>> getConsecutiveSubStrMap(Map<String, List<Integer>> suffixMap){
-		//Code changes default TreeMap ordering to key length in descending order
-		
+	private TreeMap <String, List<Integer>> getConsecutiveSubStrMap(Map<String, List<Integer>> suffixMap){
+		/* Changes default TreeMap ordering to key length in descending order */
 		TreeMap <String, List<Integer>> subStrMap = new TreeMap<String, List<Integer>>(getComparator());
 		
 		for (Map.Entry<String, List<Integer>> entry : suffixMap.entrySet()){	
 			String key = entry.getKey();
 			List<Integer> value = entry.getValue();
-			//ensure the indices are sorted in ascending value
-			Collections.sort(value);
 			
-			//substrings
-				//i.e the first index in any list of integers is by definition valid
-				//relies on List<Integer> being sorted in ascending order
-				int lastValidIndex = value.get(0); 
-				subStrMap.put(key, new ArrayList<Integer>());
-				subStrMap.get(key).add(lastValidIndex);
-				for(int i = 1; i < value.size(); i++){ // start at 1 as first entry will be correct
-					if(entry.getValue().get(i) - lastValidIndex >= key.length()){
-						lastValidIndex = value.get(i);
-						subStrMap.get(key).add(lastValidIndex);
-					} 
-				}
+			Collections.sort(value); /* Ensures the indices are sorted in ascending value */
+			int lastValidIndex = value.get(0);  /*the first index in any list of integers is by definition valid */ 
+			subStrMap.put(key, new ArrayList<Integer>());
+			subStrMap.get(key).add(lastValidIndex);
+			for(int i = 1; i < value.size(); i++){ /* start at 1 as first entry will be correct */
+				if(entry.getValue().get(i) - lastValidIndex >= key.length()){
+					lastValidIndex = value.get(i);
+					subStrMap.get(key).add(lastValidIndex);
+				} 
+			}
 		}	
 		return subStrMap;
 	}
 	
-	/**
-	 * 
-	 * Return a comparator for uses in Treemap sorted by key size in descending order
-	 */
-	//TO DO - Change comparator method to Java 8 style.
-	// Code taken from Stack Overflow (INSERT LINK)
+	
 	private Comparator<String> getComparator(){
 		Comparator<String> comp = new Comparator<String>(){
 			@Override
@@ -155,6 +144,4 @@ public class StringHierarchyAnalyser {
 		};
 		return comp;	
 	}
-				
-	
 }
